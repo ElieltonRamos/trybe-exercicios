a18fc6668bf6d6190ccb437b0ca8e57600bd503f
ElieltonRamos <elieltonramos14@gmail.com>
2023-03-29 13:54:51 -0300

    exercircios dia 1

---
+++ Fundamentos/secao-03-introducao-a-javascript/exercircio6.js
@@ -0,0 +1,14 @@
+let resultado = 'lista'
+switch (resultado) {
+    case 'aprovada':
+        console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+        break;
+    case 'lista':
+        console.log('Você está na nossa lista de espera.');
+        break;
+    case 'reprovada':
+        console.log('Infelizmente, você reprovou');
+        break;
+default:
+    console.log('dados invalidos');
+}
