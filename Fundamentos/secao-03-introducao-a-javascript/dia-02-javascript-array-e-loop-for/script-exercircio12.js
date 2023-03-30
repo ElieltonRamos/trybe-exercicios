26297c81ffe8569de8f8c200d5cf2a59b2578f3b
ElieltonRamos <elieltonramos14@gmail.com>
2023-03-30 13:36:26 -0300

    dia 2

---
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script-exercircio12.js
@@ -0,0 +1,9 @@
+let numbers = [];
+
+for (let index = 1; index <= 25; index += 1) {
+  numbers.push(index);
+}
+let numbers2 = [];
+for(index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index] / 2)
+}
