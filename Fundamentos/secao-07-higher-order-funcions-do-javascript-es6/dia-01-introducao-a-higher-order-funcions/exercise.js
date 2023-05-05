141aff075add5b236c1950275a8bafbff89974b3
ElieltonRamos <elieltonramos14@gmail.com>
2023-05-05 11:06:22 -0300

    exercircios introducao

--- Fundamentos/secao-07-higher-order-funcions-do-javascript-es6/dia-01-introducao-a-higher-order-funcions/exercise.js
+++ Fundamentos/secao-07-higher-order-funcions-do-javascript-es6/dia-01-introducao-a-higher-order-funcions/exercise.js
@@ -14,3 +14,23 @@ console.log(numbers.find((index) => index % 3 === 0 && index % 5 === 0))
 const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
 console.log(names.find((nome) => nome.length === 5))
+
+const names2 = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((nome) => name === nome);
+
+console.log(hasName(names2, 'Ana'));
+console.log(hasName(names2, 'Pedro'));
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+const verifyAges = (array, idade) => array.every((pessoa) => pessoa.age >= idade )
+  
+  console.log(verifyAges(people, 18));
+  console.log(verifyAges(people, 14));
