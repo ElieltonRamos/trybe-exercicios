e565709c5f58a44833e442d65942b3e43fc87ebe
ElieltonRamos <elieltonramos14@gmail.com>
2023-05-05 10:49:38 -0300

    exercises

--- Fundamentos/secao-07-higher-order-funcions-do-javascript-es6/dia-01-introducao-a-higher-order-funcions/exercise.js
+++ Fundamentos/secao-07-higher-order-funcions-do-javascript-es6/dia-01-introducao-a-higher-order-funcions/exercise.js
@@ -6,3 +6,11 @@ const emailListInData = [
   ];
   
 emailListInData.forEach((email) => console.log('O email '+ email + ' está cadastrado em nosso banco de dados!'))
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+console.log(numbers.find((index) => index % 3 === 0 && index % 5 === 0))
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+console.log(names.find((nome) => nome.length === 5))
