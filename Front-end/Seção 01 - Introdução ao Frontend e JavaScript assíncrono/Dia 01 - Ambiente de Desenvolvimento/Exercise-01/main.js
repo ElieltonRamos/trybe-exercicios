9da0044b38c06ac61f11e330cbe41299e47498a2
ElieltonRamos <elieltonramos14@gmail.com>
2023-05-19 15:19:43 -0300

    comecando front end

---
+++ Front-end/Seção 01 - Introdução ao Frontend e JavaScript assíncrono/Dia 01 - Ambiente de Desenvolvimento/Exercise-01/main.js
@@ -0,0 +1,16 @@
+import copy from 'clipboard-copy';
+
+import './style.css';
+
+const passwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwordBtnEl.addEventListener('click', () => {
+  const randomPassword = nanoid();
+  displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
+});
