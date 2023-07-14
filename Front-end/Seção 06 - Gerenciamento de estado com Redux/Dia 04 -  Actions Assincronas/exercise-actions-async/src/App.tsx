ebbfc66ede68e8e91adf38ca03bade54404efe54
ElieltonRamos <elieltonramos14@gmail.com>
2023-07-14 10:26:27 -0300

    ok

--- Front-end/Seção 06 - Gerenciamento de estado com Redux/Dia 04 -  Actions Assincronas/exercise-actions-async/src/App.tsx
+++ Front-end/Seção 06 - Gerenciamento de estado com Redux/Dia 04 -  Actions Assincronas/exercise-actions-async/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { Dispatch, ReduxState } from './types';
+import { fetchDogImage } from './redux/actions/actions';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const rootState = useSelector((state: ReduxState) => state);
+  const dispatch: Dispatch = useDispatch();
 
+  if (rootState.isFetching) return <h1>Carregando...</h1>
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <img src={rootState.imageURL} alt="dog" />
+      <button
+        onClick={ () => { void dispatch(fetchDogImage()) } }
+      >
+        Novo Doguinho
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
